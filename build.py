7aaa4e3e87890ba4680c693b7d1e8a0f4b370642
Gabriel Souza <gabriel@vistaonline.com.br>
2019-08-22 09:25:47 -0300

    up build

--- build.py
+++ build.py
@@ -3,4 +3,4 @@ import docker
 client = docker.from_env()
 
 client.images.build(path=".",tag="ggbr12/php-apache-dev:1.0.2")
-client.images.push(repository="ggbr12/php-apache-dev", tag="1.0.2")
+client.images.push(repository="ggbr12/php-apache-dev", tag="1.0.21234")
